fd095345bc7b1f555def616407941398a97d8a87
soyaaroncervantes <soy.aaron.cervantes@gmail.com>
2024-09-16 11:52:11 -0600

    fix(calculator-button-component): handle keyboard events for each button

--- apps/zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
+++ apps/zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '@components/calculator-button/calculator-button.component';
+import { keyEquivalent } from '../../utils/calculator.utils';
 
 @Component({
   selector: 'app-calculator',
@@ -7,5 +8,19 @@ import { CalculatorButtonComponent } from '@components/calculator-button/calcula
   templateUrl: './calculator.component.html',
   imports: [CalculatorButtonComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  buttons = viewChildren(CalculatorButtonComponent)
+  handleClick(key: string) {
+    console.log({ key });
+  }
+
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const key = keyEquivalent.get(event.key) || event.key;
+    this.handleClick(key);
+    this.buttons().forEach(button => button.keyboardPressed(key));
+  }
+}
