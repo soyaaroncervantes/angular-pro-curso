37091bb4385f06e320f442558fa526f502459e40
soyaaroncervantes <soy.aaron.cervantes@gmail.com>
2024-09-16 11:51:29 -0600

    fix(calculator-button-component): simplified logic for button component

--- apps/zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ apps/zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,21 +1,45 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  input,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { transformBooleanValue } from '../../utils/calculator.utils';
 
 @Component({
   selector: 'app-calculator-button',
   standalone: true,
   templateUrl: './calculator-button.component.html',
-  styleUrl: './calculator-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.scss',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-1/4]': '!isDoubleSize()',
+    '[class.w-2/4]': 'isDoubleSize()',
   },
 })
 export class CalculatorButtonComponent {
   isCommand = input(false, { transform: transformBooleanValue });
   isDoubleSize = input(false, { transform: transformBooleanValue });
+  onClick = output<string>();
+  protected isPressed = signal(false);
+  contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) return;
+    const value = this.contentValue()!.nativeElement.innerText.trim();
+    this.onClick.emit(value);
+  }
+
+  keyboardPressed(key: string) {
+    if (!this.contentValue()) return;
+    const value = this.contentValue()?.nativeElement.innerText.trim();
+    if (value !== key) return;
+    this.isPressed.set(true);
+    setTimeout(() => this.isPressed.set(false), 100);
   }
 }
