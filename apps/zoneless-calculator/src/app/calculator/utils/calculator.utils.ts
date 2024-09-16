c592263854b80f1e5f792562ce79d5d51e38add8
soyaaroncervantes <soy.aaron.cervantes@gmail.com>
2024-09-16 11:50:55 -0600

    fix(calculator-utils): created map value

--- apps/zoneless-calculator/src/app/calculator/utils/calculator.utils.ts
+++ apps/zoneless-calculator/src/app/calculator/utils/calculator.utils.ts
@@ -1,2 +1,10 @@
 export const transformBooleanValue = (value: boolean | string) =>
   typeof value === 'string' ? value === '' : value;
+
+export const keyEquivalent = new Map<string, string>([
+  ['/', '÷'],
+  ['*', '⨉'],
+  ['Escape', 'C'],
+  ['Clear', 'C'],
+  ['Enter', '='],
+])
