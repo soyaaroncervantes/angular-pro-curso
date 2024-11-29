6bf663cf034dbb90888ac3421bc4ae051be9c40e
soyaaroncervantes <soy.aaron.cervantes@gmail.com>
2024-11-29 07:01:05 -0600

    fix: update constant

--- apps/zoneless-calculator/src/app/calculator/utils/calculator.utils.ts
+++ apps/zoneless-calculator/src/app/calculator/utils/calculator.utils.ts
@@ -2,8 +2,8 @@ export const transformBooleanValue = (value: boolean | string) =>
   typeof value === 'string' ? value === '' : value;
 
 export const keyEquivalent = new Map<string, string>([
-  ['/', '÷'],
-  ['*', '⨉'],
+  ['÷', '/'],
+  ['⨉', '*'],
   ['Escape', 'C'],
   ['Clear', 'C'],
   ['Enter', '='],
