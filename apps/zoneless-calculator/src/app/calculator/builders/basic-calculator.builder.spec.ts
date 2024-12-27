72ddd55c8f1b9a8c8c20c910ae2d582be5baa327
soyaaroncervantes <soy.aaron.cervantes@gmail.com>
2024-12-27 14:34:37 -0600

    fix(basic-calculator-builder-spec): finished spec cases for validations and result value

--- apps/zoneless-calculator/src/app/calculator/builders/basic-calculator.builder.spec.ts
+++ apps/zoneless-calculator/src/app/calculator/builders/basic-calculator.builder.spec.ts
@@ -7,7 +7,8 @@ interface SetupTest {
 
 describe('BasicCalculatorBuilderService', () => {
   let sr: SetupTest;
-  beforeEach(() => sr = setup());
+  beforeEach(() => (sr = setup()));
+  afterEach(() => jest.clearAllMocks());
 
   it('should be created', () => expect(sr.service).toBeTruthy());
   it('should signal have default value', () => {
@@ -37,22 +38,29 @@ describe('BasicCalculatorBuilderService', () => {
       expect(sr.service.resultText()).toBe('-0');
     });
     describe('when value includes a number', () => {
+      beforeEach(() => sr.service.resultText.set('0'));
+
+      it('should set resultText, when resultText is 0', () => {
+        sr.service.validateNumber('1');
+        expect(sr.service.resultText()).toBe('1');
+        sr.service.validateNumber('2');
+        sr.service.validateNumber('3');
+        expect(sr.service.resultText()).toBe('123');
+      });
+      it('should set resultText, when resultText is -0', () => {
+        sr.service.resultText.set('-0');
+        sr.service.validateNumber('1');
+        expect(sr.service.resultText()).toBe('-1');
+        sr.service.validateNumber('2');
+        sr.service.validateNumber('3');
+        expect(sr.service.resultText()).toBe('-123');
+      });
       it('should update resultText ', () => {
         sr.service.validateNumber('1');
         expect(sr.service.resultText()).toBe('1');
         sr.service.validateNumber('2');
-        expect(sr.service.resultText()).toBe('12');
-      });
-      it('should set resultText, when resultText is 0', () => {
-        expect(sr.service.resultText()).toBe('0');
-        sr.service.validateNumber('1');
-        expect(sr.service.resultText()).toBe('1');
-      });
-      it('should set resultText, when resultText is -0', () => {
-        expect(sr.service.resultText()).toBe('0');
-        sr.service.resultText.set('-0');
-        sr.service.validateNumber('1');
-        expect(sr.service.resultText()).toBe('-1');
+        sr.service.validateNumber('3');
+        expect(sr.service.resultText()).toBe('123');
       });
     });
   });
@@ -62,6 +70,11 @@ describe('BasicCalculatorBuilderService', () => {
     it('should keep default operator, when operator is not included', () => {
       sr.service.validateOperators('(');
       expect(sr.service.lastOperator()).toBe('+');
+      sr.service.validateOperators('*');
+      expect(sr.service.lastOperator()).toBe('*');
+      sr.service.validateOperators('(');
+      expect(sr.service.lastOperator()).not.toBe('+');
+      expect(sr.service.lastOperator()).toBe('*');
     });
 
     it('should update lastOperator, when operator is included', () => {
@@ -69,6 +82,10 @@ describe('BasicCalculatorBuilderService', () => {
       expect(sr.service.subResultText()).toBe('30');
       expect(sr.service.resultText()).toBe('0');
       expect(sr.service.lastOperator()).not.toBe('+');
+      sr.service.validateOperators('-');
+      expect(sr.service.lastOperator()).toBe('-');
+      expect(sr.service.resultText()).toBe('0');
+      expect(sr.service.subResultText()).toBe('0');
     });
   });
   describe('when validateValue is raised', () => {
@@ -82,6 +99,86 @@ describe('BasicCalculatorBuilderService', () => {
       sr.service.resultText.set('1234567890');
       sr.service.validateValue('1');
       expect(console.warn).toHaveBeenCalled();
+    });
+  });
+  describe('when validateSpecialOperators is raised', () => {
+    it('should call calculateResult when value is =', () => {
+      const spy = jest.spyOn(sr.service, 'calculateResult');
+      sr.service.validateSpecialOperators('=');
+      expect(spy).toHaveBeenCalled();
+    });
+    describe('when value is Backspace', () => {
+      afterEach(() => {
+        sr.service.validateSpecialOperators('Backspace');
+        expect(sr.service.resultText()).toBe('0');
+      });
+
+      it('should return when resultText is 0', () => {
+        sr.service.validateSpecialOperators('Backspace');
+        expect(sr.service.resultText()).toBe('0');
+        sr.service.subResultText.set('100');
+        sr.service.lastOperator.set('*');
+      });
+
+      it('should resultText set to 0, when resultText has 2 characters and a character is -', () =>
+        sr.service.resultText.set('-3'));
+
+      it('should resultText set to 0, when resultText has 1 character', () =>
+        sr.service.resultText.set('3'));
+
+      it('should slice resultText by 1, when resultText has 2 characters', () => {
+        sr.service.resultText.set('-125');
+        sr.service.validateSpecialOperators('Backspace');
+        expect(sr.service.resultText()).toBe('-12');
+        sr.service.validateSpecialOperators('Backspace');
+        expect(sr.service.resultText()).toBe('-1');
+      });
+    });
+    describe('when value is +/-', () => {
+      beforeEach(() => sr.service.resultText.set('12'));
+      it('should add negative sign, when resultText does not contain it', () => {
+        sr.service.validateSpecialOperators('+/-');
+        expect(sr.service.resultText()).toBe('-12');
+      });
+      it('should remove negative sign, when resultText contains it', () => {
+        sr.service.validateSpecialOperators('+/-');
+        expect(sr.service.resultText()).toBe('12');
+      });
+    })
+  });
+  describe('when calculateResult is raised', () => {
+    beforeEach( () => {
+      sr.service.subResultText.set('12');
+      sr.service.resultText.set('2');
+    })
+    afterEach(() => {
+      expect(typeof sr.service.resultText()).toBe('string');
+      expect(sr.service.subResultText()).toBe('0');
+    })
+    it('should sum 12 and 2, when lastOperator is +', () => {
+      sr.service.lastOperator.set('+');
+      sr.service.calculateResult();
+      expect(sr.service.resultText()).toBe('14');
+    });
+    it('should subtract 12 and 2, when lastOperator is -', () => {
+      sr.service.lastOperator.set('-');
+      sr.service.calculateResult();
+      expect(sr.service.resultText()).toBe('10');
+    });
+    it('should multiply 12 and 2, when lastOperator is *', () => {
+      sr.service.lastOperator.set('*');
+      sr.service.calculateResult();
+      expect(sr.service.resultText()).toBe('24');
+    });
+    it('should divide 12 and 2, when lastOperator is /', () => {
+      sr.service.lastOperator.set('/');
+      sr.service.calculateResult();
+      expect(sr.service.resultText()).toBe('6');
+    });
+    it('should result be zero, when lastOperator is not a valid operator', () => {
+      sr.service.lastOperator.set('^');
+      sr.service.calculateResult();
+      expect(sr.service.resultText()).toBe('0');
     });
   });
 });
