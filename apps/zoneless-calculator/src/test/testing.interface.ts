7cbd9d622aa4d70602a5386bb9b96a0b9701cd98
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-09 19:15:01 -0600

    fix: added interface for routing harness

--- apps/zoneless-calculator/src/test/testing.interface.ts
+++ apps/zoneless-calculator/src/test/testing.interface.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture } from '@angular/core/testing';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 export interface ComponentTestInterface<T> {
   fixture: ComponentFixture<T>;
   component: T;
+}
+
+export interface RoutingTestInterface<T> {
+  harness: RouterTestingHarness,
+  component: T
 }
