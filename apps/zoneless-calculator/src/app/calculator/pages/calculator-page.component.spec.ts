835abaa2a7ded154e30ca9ec1233ec1ede3204bf
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-15 07:25:24 -0600

    fix(calculator-page.component-spec): finished test for component page

--- apps/zoneless-calculator/src/app/calculator/pages/calculator-page.component.spec.ts
+++ apps/zoneless-calculator/src/app/calculator/pages/calculator-page.component.spec.ts
@@ -1,25 +1,43 @@
-import { ComponentTestInterface } from '@test/testing.interface';
-import CalculatorPageComponent from './calculator-page.component';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { RoutingTestInterface } from '@test/testing.interface';
+import CalculatorPageComponent from './calculator-page.component';
+import { Nullable } from '../utils/types.utils';
+import { CalculatorComponent } from '../components/calculator/calculator.component';
 
-interface SetupTest extends ComponentTestInterface<CalculatorPageComponent> {
-  element: HTMLDivElement;
+interface SetupTest extends RoutingTestInterface<CalculatorPageComponent> {
+  element: Nullable<HTMLElement>;
 }
 
 describe('CalculatorPageComponent', () => {
-  let sr: SetupTest;
+  let sr: RoutingTestInterface<CalculatorPageComponent>;
   beforeEach(async () => (sr = await setup()));
 
   it('should create calculator page', () => expect(sr.component).toBeTruthy());
+  fit('should have a calculator component', () => {
+    const calculatorComponent = sr.harness.routeDebugElement?.query(By.directive(CalculatorComponent));
+    expect(calculatorComponent).toBeTruthy();
+  });
 });
 
 async function setup(): Promise<SetupTest> {
+  const mockRoutes = [
+    {
+      path: 'calculator',
+      loadComponent: () => import('./calculator-page.component'),
+    },
+  ];
   await TestBed.configureTestingModule({
-    imports: [CalculatorPageComponent],
+    providers: [provideRouter(mockRoutes)],
   }).compileComponents();
-  const fixture = TestBed.createComponent(CalculatorPageComponent);
-  const component = fixture.componentInstance;
-  const element = fixture.nativeElement as HTMLDivElement;
-  fixture.detectChanges();
-  return { fixture, component, element };
+  const harness = await RouterTestingHarness.create();
+  const component = await harness.navigateByUrl(
+    '/calculator',
+    CalculatorPageComponent
+  );
+  const element = harness.routeNativeElement;
+  harness.detectChanges();
+  return { harness, component, element };
 }
