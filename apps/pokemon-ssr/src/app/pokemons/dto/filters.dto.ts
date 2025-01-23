ef1580c1cb0da89ef22cebee80ed13064e86e918
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-23 17:32:22 -0600

    fix(filterDto): created new dto

--- apps/pokemon-ssr/src/app/pokemons/dto/filters.dto.ts
+++ apps/pokemon-ssr/src/app/pokemons/dto/filters.dto.ts
@@ -1,4 +1,11 @@
 import { NamedResourceDto, UnnamedResourceDto } from './resource.dto';
+import { AutoMap } from '@automapper/classes';
 
 export type NamedFilterDto = NamedResourceDto;
 export type UnnamedFilterDto = UnnamedResourceDto;
+
+export class BaseFilterDto {
+  @AutoMap(() => Number) limit!: number;
+  @AutoMap(() => Number) offset!: number;
+
+}
