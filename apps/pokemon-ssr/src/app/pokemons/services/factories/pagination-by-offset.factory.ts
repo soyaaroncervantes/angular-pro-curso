48b2be121728af741477f8ebb577ec4e017e73ca
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-31 17:54:33 -0600

    feat: created new pagination factory

---
+++ apps/pokemon-ssr/src/app/pokemons/services/factories/pagination-by-offset.factory.ts
@@ -0,0 +1,70 @@
+import {
+  PaginationAbstractFactory,
+  PaginationByOffset,
+} from '../../../core/pagination/pagination-abstract-factory';
+import { FilterModel } from '../../models/filter.model';
+import {
+  PaginationByOffsetModel,
+  PaginationModel,
+} from '../../models/pagination.model';
+import { Observable } from 'rxjs';
+import { BaseListModel } from '../../models/list.model';
+import { DestroyRef } from '@angular/core';
+
+interface Pagination<A> {
+  offset: number;
+  limit: number;
+  request: A;
+}
+
+export class PaginationByOffsetFactory<
+  A,
+  B extends FilterModel
+> extends PaginationAbstractFactory<A, B> {
+  private offset = 0;
+
+  constructor(
+    protected override readonly destroyRef: DestroyRef,
+    protected override paginationInstance: PaginationByOffset<A, B>,
+    protected override fetchFn: (pagination: PaginationByOffsetModel<B>) => Observable<BaseListModel<A>>,
+    protected override itemsPerPage = 90
+  ) {
+    super(destroyRef, paginationInstance, fetchFn, itemsPerPage);
+  }
+
+  nextPage(reset = false): void {
+    if (!this.paginationInstance.request$.value) return;
+    if (!this.hasNextPage || !this.fetchingState$.closed) return;
+
+    if (reset) {
+      this.offset = 0;
+      this.paginationInstance.list$.next(null);
+    }
+
+    this.fetchingState$.unsubscribe();
+    this.loading$.next(true);
+    const request = this.paginationInstance.request$.value;
+    const pagination = this.createPagination({
+      request,
+      limit: this.itemsPerPage,
+      offset: this.offset,
+    });
+
+    this.fetchingState$ = this.fetchFn(pagination).subscribe((list) => {
+      const currentElements = reset
+        ? []
+        : ((this.paginationInstance.list$?.value ?? []) as BaseListModel<A>[]);
+      console.log({ currentElements });
+      this.offset += this.itemsPerPage;
+      this.loading$.next(false);
+    });
+  }
+
+  private createPagination<A>({
+    request,
+    limit,
+    offset,
+  }: Pagination<A>): PaginationModel<A> {
+    return new PaginationByOffsetModel(request, limit, offset);
+  }
+}
--- apps/pokemon-ssr/src/app/pokemons/services/factories/pokemon-api.factory.ts
+++ apps/pokemon-ssr/src/app/pokemons/services/factories/pokemon-api.factory.ts
@@ -2,25 +2,26 @@ import { Injectable } from '@angular/core';
 import {
   ApiAbstractFactory,
   type APIResponseDto,
-  type HttpOptions
+  type HttpOptions,
 } from '../../../core/api/api-abstract-factory';
 import { FilterDto } from '../../dto/filters.dto';
 import { PaginationByOffsetDto } from '../../dto/pagination.dto';
 
 export interface PokemonResourceList {
-  getList$<A, B extends PaginationByOffsetDto<C>, C extends FilterDto> (
+  getList$<A, B extends PaginationByOffsetDto<C>, C extends FilterDto>(
     path: string,
-    filters?: B,
-  ): APIResponseDto<A>
+    filters?: B
+  ): APIResponseDto<A>;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonApiFactory extends ApiAbstractFactory {
-  protected url = 'https://pokeapi.co/api/v2';
-  get<T>(url: string, options?: HttpOptions): APIResponseDto<T> {
-    return this.http.get<T>(this.createURL(url), {
+  protected readonly url = 'https://pokeapi.co/api/v2';
+
+  get<T>(path: string, options?: HttpOptions): APIResponseDto<T> {
+    return this.http.get<T>(this.createURL(path), {
       ...options,
       observe: 'response' as const,
     });
