2c9397c615cac73904eb3036e37c9bd16e82e5c7
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-31 17:52:16 -0600

    fix(pagination-model): added missing constructor

--- apps/pokemon-ssr/src/app/pokemons/models/pagination.model.ts
+++ apps/pokemon-ssr/src/app/pokemons/models/pagination.model.ts
@@ -3,8 +3,18 @@ import { AutoMap } from '@automapper/classes';
 export class PaginationModel<T> {
   @AutoMap(() => Object) request!: T;
   @AutoMap(() => Number) limit: number = 90;
+
+  constructor(request: T, limit: number) {
+    this.request = request;
+    this.limit = limit;
+  }
 }
 
 export class PaginationByOffsetModel<T> extends PaginationModel<T> {
-  @AutoMap(() => Number) offset: number = 0;
+  @AutoMap(() => Number) offset? = 0;
+
+  constructor(request: T, limit: number, offset: number) {
+    super(request, limit);
+    this.offset = offset;
+  }
 }
