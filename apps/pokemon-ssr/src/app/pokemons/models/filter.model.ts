68a5e4627044315e258d182ae652ec0589ff7abb
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-01-31 17:54:55 -0600

    fix: added constructor with ParamMap

--- apps/pokemon-ssr/src/app/pokemons/models/filter.model.ts
+++ apps/pokemon-ssr/src/app/pokemons/models/filter.model.ts
@@ -1,7 +1,11 @@
-import { Params } from '@angular/router';
+import { ParamMap, Params } from '@angular/router';
 import { PaginationByOffsetModel } from './pagination.model';
 
 export class FilterModel implements Params {}
-export class PaginationFilterModel extends FilterModel {}
+export class PaginationFilterModel extends FilterModel {
+  constructor(params: ParamMap) {
+    super();
+  }
+}
 
 export class PokemonPaginationFilterModel extends PaginationByOffsetModel<PaginationFilterModel> {}
