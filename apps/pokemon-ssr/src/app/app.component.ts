23ab4d30958e1f38a0a0344fe21d18c3728d397a
Aaron Cervantes <soyaaroncervantes@gmail.com>
2025-02-01 00:23:11 -0600

    fix(navbar-component): moved inside own folder

--- apps/pokemon-ssr/src/app/app.component.ts
+++ apps/pokemon-ssr/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './core/components/navbar.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @Component({
   imports: [RouterModule, NavbarComponent],
