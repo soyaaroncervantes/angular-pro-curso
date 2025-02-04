816d18ab4f46c29e827818e74e8903de74c46d91
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2025-02-04 00:39:23 -0600

    fix(pokemon-card-component): erased unused imports

--- apps/pokemon-ssr/src/app/pokemons/components/card/pokemon-card.component.ts
+++ apps/pokemon-ssr/src/app/pokemons/components/card/pokemon-card.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { NamedAPIResourceModel } from '../../models/utility.model';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgClass],
+  imports: [],
   host: {
     class: 'flex gap-4 border rounded-xl overflow-auto',
   },
